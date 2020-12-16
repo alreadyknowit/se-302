2b50d5c9ea1f534b1a732817ddef75205f9c2983
ErdemFuatYazici <72793248+ErdemFuatYazici@users.noreply.github.com>
2020-12-16 09:03:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace SE_302_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This part is just to see that this code is wworking or not
+            Syllabus s = new Syllabus("http://dm.ieu.edu.tr/tr/syllabus/type/read/id/CE+223");
+            s.writenode();
+        }
+    }
+}
---
+++ Syllabus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml;
+using HtmlAgilityPack;
+
+public class Syllabus
+{
+    HtmlNode node;
+
+	//I find it easier to attach a String link to go and extract html file of a website
+	public Syllabus(String link)
+	{
+		var html = @link;
+		HtmlWeb web = new HtmlWeb();
+        var doc = web.Load(html);
+		node = doc.DocumentNode.SelectSingleNode("//body");
+	}
+
+	// This part is totally erasable becasue it is just for us to see that code is running properly
+	public void writenode()
+    {
+		Console.WriteLine("Node Name: " + node.Name + "\n" + node.OuterHtml);
+	}
+
+}
